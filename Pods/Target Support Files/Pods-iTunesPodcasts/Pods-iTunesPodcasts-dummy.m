a108f53d3d89fc8d98c78235ea8befb4e10504a5
Florin Uscatu <florinusc@gmail.com>
2022-05-27 14:05:11 +0300

    Remove pod files

--- Pods/Target Support Files/Pods-iTunesPodcasts/Pods-iTunesPodcasts-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_iTunesPodcasts : NSObject
-@end
-@implementation PodsDummy_Pods_iTunesPodcasts
-@end
--- Pods/Target Support Files/Pods-iTunesPodcastsTests/Pods-iTunesPodcastsTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_iTunesPodcastsTests : NSObject
-@end
-@implementation PodsDummy_Pods_iTunesPodcastsTests
-@end
